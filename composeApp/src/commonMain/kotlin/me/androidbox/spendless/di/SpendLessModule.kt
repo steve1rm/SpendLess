e4da3698fe798c224874ab2ef900fb3f9536321d
graial <al@paglalayag.net>
2025-02-21 08:10:27 +0800

    resolve module conflict

--- composeApp/src/commonMain/kotlin/me/androidbox/spendless/di/SpendLessModule.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/spendless/di/SpendLessModule.kt
@@ -7,6 +7,9 @@ import me.androidbox.spendless.transactions.data.RepositoryImp
 import me.androidbox.spendless.transactions.domain.FetchAllTransactionsUseCase
 import me.androidbox.spendless.transactions.domain.FetchAllTransactionsUseCaseImp
 import me.androidbox.spendless.transactions.domain.Repository
+import me.androidbox.spendless.data.SpendLessDataSource
+import me.androidbox.spendless.data.SpendLessDataSourceImpl
+import me.androidbox.spendless.data.SpendLessDatabase
 import org.koin.core.module.dsl.factoryOf
 import me.androidbox.spendless.data.SpendLessDataSource
 import me.androidbox.spendless.data.SpendLessDataSourceImpl
@@ -24,6 +27,12 @@ val spendLessModule = module {
     viewModelOf(::PinViewModel)
     viewModelOf(::TransactionViewModel)
     viewModelOf(::DashBoardViewModel)
+
+    factory<SpendLessDataSource> {
+        SpendLessDataSourceImpl(
+            get<SpendLessDatabase>()
+        )
+    }
 
     factory<Repository> {
         RepositoryImp()
