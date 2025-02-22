b7eda2f3171a28fc99d22a32a876cc4396d4ac5a
graial <al@paglalayag.net>
2025-02-22 11:40:39 +0800

    add user table

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/spendless/data/UserDao.kt
@@ -0,0 +1,18 @@
+package me.androidbox.spendless.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user WHERE username='%'")
+    suspend fun getUser(): User
+
+    @Insert
+    suspend fun insertAll(vararg user: User)
+
+//    @Delete
+//    fun delete(transaction: Transaction)
+}
