04e57380323838bcbee9b3c683b6379e358a7ec8
graial <al@paglalayag.net>
2025-03-02 03:46:15 +0800

    rebase to master

--- composeApp/src/androidMain/kotlin/me/androidbox/spendless/androidSpecificModule.kt
+++ composeApp/src/androidMain/kotlin/me/androidbox/spendless/androidSpecificModule.kt
@@ -7,14 +7,8 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val androidSpecificModule = module {
-<<<<<<< HEAD
     single<SpendLessDatabase> {
         val dbFile = androidContext().getDatabasePath("spendLess.db")
-=======
-
-    single<SpendLessDatabase> {
-        val dbFile = androidContext().getDatabasePath("spendLessJournal.db")
->>>>>>> 1f1e1d14dbd8567ee7b1093a285e279d5c2cb016
 
         Room.databaseBuilder<SpendLessDatabase>(
             context = androidContext(),
