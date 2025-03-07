6f2692e053c526633493fcbed2c26569ef2b4946
steve.mason <steve.mason@sainsburys.co.uk>
2025-03-07 14:14:22 +0000

    Adds register screen keyboard actions

--- composeApp/src/commonMain/kotlin/me/androidbox/spendless/authentication/presentation/screens/RegisterScreen.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/spendless/authentication/presentation/screens/RegisterScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Button
@@ -32,8 +34,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,6 +59,8 @@ fun RegisterScreen(
     registerState: RegisterState,
     action: (action: RegisterAction) -> Unit
 ) {
+
+    val keyboard = LocalSoftwareKeyboardController.current
 
     var usernameBorderColor by remember {
         mutableStateOf(Color.Transparent)
@@ -144,7 +151,15 @@ fun RegisterScreen(
                         color = OnSurface.copy(alpha = 0.6f),
                         fontWeight = FontWeight.W400
                     )
-                }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboard?.hide()
+                    }
+                )
             )
 
             Spacer(modifier = Modifier.height(16.dp))
